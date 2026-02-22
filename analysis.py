d2d702df8b63dace3e09df76c704d32e9a55029b
olgamira931-spec <olgamira931@gmail.com>
2026-02-22 18:26:35 +0200

    update analysis file

--- analysis.py
+++ analysis.py
@@ -17,3 +17,13 @@ print("Продажі по містах:")
 print(df)
 
 print("Середнє значення:", df["sales"].mean())
+print("Максимальні продажі:", df["sales"].max())
+import pandas as pd
+
+data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 900, 500]}
+df = pd.DataFrame(data)
+
+print("Продажі по містах:")
+print(df)
+
+print("Середнє значення:", df["sales"].mean())
