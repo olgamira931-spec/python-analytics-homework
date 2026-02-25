2b36df32f84c6274322917fc33e71f88a31a54f6
olgamira931-spec <olgamira931@gmail.com>
2026-02-25 11:06:58 +0200

    Added analysis script for homework

--- analysis.py
+++ analysis.py
@@ -1,15 +1,9 @@
-import unittest
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
 
-if __name__ == '__main__':
-    unittest.main()
+
 import pandas as pd
-
 data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 900, 500]}
 df = pd.DataFrame(data)
 
