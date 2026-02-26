55af95384e6bf3c92e8e55d6efc0d32ecda4c8eb
olgamira931-spec <olgamira931@gmail.com>
2026-02-26 19:58:31 +0200

    add average sales comment

--- analysis.py
+++ analysis.py
@@ -3,15 +3,9 @@
 
 
 
-import pandas as pd
-data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 900, 500]}
-df = pd.DataFrame(data)
 
-print("Продажі по містах:")
-print(df)
 
-print("Середнє значення:", df["sales"].mean())
-print("Максимальні продажі:", df["sales"].max())
+
 import pandas as pd
 
 data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 900, 500]}
